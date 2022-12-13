b6e181be2ce9ff87c6fb215f60584fc3126b419d
yegane78 <74728966+yegane78@users.noreply.github.com>
2022-12-13 19:58:17 +0330

    Add files via upload

---
+++ test8.py
@@ -0,0 +1,8 @@
+import math
+a=int(input("please enter zele aval = "))
+b=int(input("please enter zele dovom = "))
+c=int(input("please enter zele sevom = "))
+if  a+b>c and b+c>a and a+c>b:
+    print("it is triangle ! ")
+else:
+    print("it is not triangle !")
